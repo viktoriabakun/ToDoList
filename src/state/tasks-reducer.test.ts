1dcf83f369ce1031d16e3470f92e554e06ced5ad
viktoriabakun <viktoriabakun00@gmail.com>
2020-11-17 13:11:51 +0300

    lesson 9: remove task & add task reducers and tests are ready

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,17 +1,17 @@
-import {removeTaskAC, tasksReducer} from './tasks-reducer';
+import {addTaskAC, removeTaskAC, tasksReducer} from './tasks-reducer';
 import {TasksStateType} from '../App';
 
 test('correct task should be deleted from correct array', () => {
     const startState: TasksStateType = {
         "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
+            {id: "1", title: "CSS", isDone: false},
+            {id: "2", title: "JS", isDone: true},
+            {id: "3", title: "React", isDone: false}
         ],
         "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "2", title: "milk", isDone: true },
-            { id: "3", title: "tea", isDone: false }
+            {id: "1", title: "bread", isDone: false},
+            {id: "2", title: "milk", isDone: true},
+            {id: "3", title: "tea", isDone: false}
         ]
     };
 
@@ -21,14 +21,54 @@ test('correct task should be deleted from correct array', () => {
 
     expect(endState).toEqual({
         "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
+            {id: "1", title: "CSS", isDone: false},
+            {id: "2", title: "JS", isDone: true},
+            {id: "3", title: "React", isDone: false}
         ],
         "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "3", title: "tea", isDone: false }
+            {id: "1", title: "bread", isDone: false},
+            {id: "3", title: "tea", isDone: false}
+        ]
+    });
+});
+
+test('correct task should be added to correct array', () => {
+
+
+    const startState: TasksStateType = {
+        "todolistId1": [
+            {id: "1", title: "CSS", isDone: false},
+            {id: "2", title: "JS", isDone: true},
+            {id: "3", title: "React", isDone: false}
+        ],
+        "todolistId2": [
+            {id: "1", title: "bread", isDone: false},
+            {id: "2", title: "milk", isDone: true},
+            {id: "3", title: "tea", isDone: false}
+        ]
+    };
+
+    const action = addTaskAC("juice", "todolistId2");
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState).toEqual({
+        "todolistId1": [
+            {id: "1", title: "CSS", isDone: false},
+            {id: "2", title: "JS", isDone: true},
+            {id: "3", title: "React", isDone: false}
+        ],
+        "todolistId2": [
+            {id: action.taskId, title: 'juice', isDone: false},
+            {id: "1", title: "bread", isDone: false},
+            {id: "2", title: "milk", isDone: true},
+            {id: "3", title: "tea", isDone: false}
         ]
     });
 
+    // expect(endState["todolistId1"].length).toBe(XXX);
+    // expect(endState["todolistId2"].length).toBe(XXX);
+    // expect(endState["todolistId2"][0].id).toBeDefined();
+    // expect(endState["todolistId2"][0].title).toBe(XXX);
+    // expect(endState["todolistId2"][0].isDone).toBe(XXX);
 });
--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,16 +1,33 @@
 import {TasksStateType} from "../App";
+import {TasksType} from "../Todolist";
+import {v1} from "uuid";
 
-export type ActionsType = ReturnType<typeof removeTaskAC>
+export type ActionsType =
+    | ReturnType<typeof removeTaskAC>
+    | ReturnType<typeof addTaskAC>
+
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
     switch (action.type) {
-        case 'REMOVE-TASK':
-            return {
-                ...state,
-                [action.todolistId]: state[action.todolistId].
-                filter(t => t.id !== action.taskId)
-            };
 
+        case 'REMOVE-TASK':
+            const stateCopy = {...state}
+            stateCopy[action.todolistId] = state[action.todolistId].filter(
+                t => t.id !== action.taskId)
+            return stateCopy
+
+        case "ADD-TASK": {
+            const stateCopy = {...state}
+            const tasks = stateCopy[action.todolistId]
+            const newTask: TasksType = {
+                id: action.taskId,
+                title: action.title,
+                isDone: false
+            }
+            const tasksCopy = [newTask,...tasks]
+            stateCopy[action.todolistId] = tasksCopy
+            return stateCopy
+        }
         default:
             throw new Error("I don't understand this type of action")
     }
@@ -18,6 +35,10 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
 
 export const removeTaskAC = (taskId: string, todolistId: string) => {
     return {type: 'REMOVE-TASK', taskId, todolistId} as const
+}
+
+export const addTaskAC = (title: string, todolistId: string) => {
+    return {type: 'ADD-TASK', title, todolistId, taskId: v1()} as const
 }
 
 
