fb080a732b6ed9c084b339e3e062dec533a72a10
viktoriabakun <viktoriabakun00@gmail.com>
2020-11-17 15:05:52 +0300

    lesson 9: change-task-status reducer ready

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -5,6 +5,7 @@ import {v1} from "uuid";
 export type ActionsType =
     | ReturnType<typeof removeTaskAC>
     | ReturnType<typeof addTaskAC>
+    | ReturnType<typeof changeTaskStatusAC>
 
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
@@ -24,10 +25,22 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
                 title: action.title,
                 isDone: false
             }
-            const tasksCopy = [newTask,...tasks]
+            const tasksCopy = [newTask, ...tasks]
             stateCopy[action.todolistId] = tasksCopy
             return stateCopy
         }
+
+        case "CHANGE-TASK-STATUS": {
+            const stateCopy = {...state}
+            const tasks = state[action.todolistId]
+
+            const tasksCopy = tasks
+                .map(t => t.id !== action.taskId ? t : {...t, isDone: action.status})
+
+            stateCopy[action.todolistId] = tasksCopy
+            return stateCopy
+        }
+
         default:
             throw new Error("I don't understand this type of action")
     }
@@ -41,7 +54,9 @@ export const addTaskAC = (title: string, todolistId: string) => {
     return {type: 'ADD-TASK', title, todolistId, taskId: v1()} as const
 }
 
-
+export const changeTaskStatusAC = (taskId: string, status: boolean, todolistId: string) => {
+    return {type: 'CHANGE-TASK-STATUS', taskId, status, todolistId} as const
+}
 
 
 
