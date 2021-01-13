3087faed8ceda41bac040db4a36e6e2c336f6c26
viktoriabakun <viktoriabakun00@gmail.com>
2021-01-13 23:16:09 +0300

    lesson 13: start

--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import {Provider} from "react-redux";
-import {AppRootStateType, store} from "../../state/store";
-import {combineReducers, createStore} from "redux";
-import {tasksReducer} from "../../state/tasks-reducer";
-import {todolistsReducer} from "../../state/todolists-reducer";
-import {v1} from "uuid";
+import React from 'react'
+import {Provider} from 'react-redux'
+import {combineReducers, createStore} from 'redux'
+import {tasksReducer} from '../../state/tasks-reducer'
+import {todolistsReducer} from '../../state/todolists-reducer'
+import {v1} from 'uuid'
+import {AppRootStateType} from '../../state/store'
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
@@ -28,10 +28,7 @@ const initialGlobalState = {
     }
 };
 
-
-
 export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootStateType);
-
 
 export const ReduxStoreProviderDecorator = (storyFn: any) => (
     <Provider
